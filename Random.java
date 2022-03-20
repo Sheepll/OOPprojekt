9203c115b7595a40559f71b414f8beca423489fc
Liisi Voitra <liisivoitra@gmail.com>
2022-03-20 14:50:42 +0200

    Random klassi täiendus

--- Random.java
+++ Random.java
@@ -1,4 +1,11 @@
 package OOPprojekt;
+import java.lang.Math;
 
 public class Random {
-}
+    public static void main(String[] args){
+        int min = 1;
+        int max = 10;
+        int juhuslikNUmber = (int)(Math.random() * (max - min + 1) + min);
+        System.out.println("Suvaline nr: " + juhuslikNUmber);
+    }
+}//klass
