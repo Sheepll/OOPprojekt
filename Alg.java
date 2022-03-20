58545bef4127368628b686a2138fe498464aa646
Sheepll <laurakoobas@gmail.com>
2022-03-20 14:30:23 +0200

    hello

--- Alg.java
+++ Alg.java
@@ -1,4 +1,8 @@
+import javax.swing.JOptionPane;
 public class Alg {
-    //hello
+    public static void main(String[] args) {
+        String karakteriNimi = JOptionPane.showInputDialog(null, "Sisestage oma karakteri nimi","Nimi",JOptionPane.QUESTION_MESSAGE);
+        System.out.println(karakteriNimi);
+    }
 }
 
