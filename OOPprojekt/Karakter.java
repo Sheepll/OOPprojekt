097c1e4a740dc45160c596c9c26dde0e9fddf476
Sheepll <laurakoobas@gmail.com>
2022-03-20 14:55:31 +0200

    karakter update

--- OOPprojekt/Karakter.java
+++ OOPprojekt/Karakter.java
@@ -4,14 +4,49 @@ public class Karakter {
     private double strengthModifer;
     private int healthModifier;
     private String nimi;
-    private int baseHealth = 50;
-    private double baseStrength = 1.0;
+    private int currentHealth = 50;
+    private double currentStrength = 1.0;
+    int currentLevel = 1;
 
     public int getBaseHealth() {
-        return baseHealth;
+        return currentHealth;
     }
 
-    public void setBaseHealth(int baseHealth) {
-        this.baseHealth = baseHealth;
+    public double getBaseStrength() { return currentStrength;}
+
+    public String getNimi() { return nimi;}
+
+    public void setNimi(String nimi) { this.nimi = nimi;}
+
+    public int getHealthModifier() { return healthModifier;}
+
+    public void setHealthModifier(int healthModifier) { this.healthModifier = healthModifier;}
+
+    public double getStrengthModifer() { return strengthModifer;}
+
+    public void setStrengthModifer(double strengthModifer) { this.strengthModifer = strengthModifer;}
+
+    public int getCurrentLevel() {
+        return currentLevel;
+    }
+
+    public void setCurrentLevel(int currentLevel) {
+        this.currentLevel = currentLevel;
+    }
+
+    public Karakter(double strengthModifer, int healthModifier, String nimi, int currentHealth, double currentStrength, int currentLevel) {
+        this.strengthModifer = strengthModifer;
+        this.currentHealth = currentHealth;
+        this.currentStrength = currentStrength;
+        this.healthModifier = healthModifier;
+        this.nimi = nimi;
+        this.currentLevel = currentLevel;
+    }
+
+    void healthUp(){
+        currentHealth = currentLevel+currentHealth;
+    }
+    void strengthUp(){
+        currentStrength = currentLevel*0.2 + currentStrength;
     }
 }
