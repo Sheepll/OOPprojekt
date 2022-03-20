175faf66a5d924ddfa847a7a0d47ebb7f99b33af
Sheepll <laurakoobas@gmail.com>
2022-03-20 14:33:34 +0200

    test1

--- Alg.java
+++ Alg.java
@@ -3,6 +3,7 @@ public class Alg {
     public static void main(String[] args) {
         String karakteriNimi = JOptionPane.showInputDialog(null, "Sisestage oma karakteri nimi","Nimi",JOptionPane.QUESTION_MESSAGE);
         System.out.println(karakteriNimi);
+        //test
     }
 }
 
